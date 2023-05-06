93fc37ab9f87d97efd5047571b01aa8c669a6c9f
Radman R <rdmnr@protonmail.com>
2023-05-06 14:57:12 -0700

    implement h-bond functions

--- data_utils.py
+++ data_utils.py
@@ -3,6 +3,8 @@ import pandas as pd
 from pyrosetta import *
 from pyrosetta.rosetta.protocols.simple_moves import MutateResidue
 from pyrosetta.rosetta.protocols.membrane import get_secstruct
+from pyrosetta.rosetta.core.scoring.hbonds import HBondSet
+
 
 
 # initializing PyRosetta
@@ -15,12 +17,14 @@ def single_mutation_analysis(wt_pose):
     Creates all possible single AA mutation when given a wildtype pose
     """
     
+    
+    # Creating the pandas data structure
     df = pd.DataFrame(columns=['type', # wild_type or mutant
                                'residue_number', # the number of the targeted residue
                                'previous_aa', # Previous amino acid (in the wildtype)
                                'new_aa_1l', # the 1 letter representation of the new amino acid
                                'new_aa_3l', # the 3 letter representation of the new amino acid
-                               'conversion' # resiude number + previous aa + new aa i.e. 3AtoR (at resiude number 3 A was converted to R)
+                               'conversion', # resiude number + previous aa + new aa i.e. 3AtoR (at resiude number 3 A was converted to R)
                                'new_seq', # the new amino acid sequence after the mutation has been applied (if it is mutant otherwise it is wildtype)
                                'fa_score', # the Full Atom score for the new amino acid
                                'ddg_score', # the ddG (change in binding free energy) score
@@ -29,6 +33,34 @@ def single_mutation_analysis(wt_pose):
                                'sasa_score', # solvent accessible surface area
                                'secondary_structure'
                                ])
+    
+    # adding the wildtype in the 1st row of the data frame
+    df.loc[len(df)] = ['wild_type', 'NA', 'NA', 'NA',  'NA',  'NA', wt_pose.sequence(), calculate_FA_score(wt_pose),
+                               'ddg_energy', # the ddG (change in binding free energy)
+                               calculate_hbonds_comprehensive(wt_pose), # hydrogen bonding patterns
+                               'gyration_radius', # radius of gyration is a measure of the protein's compactness.
+                               'sasa_score', # solvent accessible surface area
+                               'secondary_structure'
+                               ]
+    # df.loc[len(df)] = ['wild_type', # wild_type or mutant
+    #                            'NA', # the number of the targeted residue
+    #                            '', # Previous amino acid (in the wildtype)
+    #                            'new_aa_1l', # the 1 letter representation of the new amino acid
+    #                            'new_aa_3l', # the 3 letter representation of the new amino acid
+    #                            'conversion' # resiude number + previous aa + new aa i.e. 3AtoR (at resiude number 3 A was converted to R)
+    #                            'new_seq', # the new amino acid sequence after the mutation has been applied (if it is mutant otherwise it is wildtype)
+    #                            'fa_score', # the Full Atom score for the new amino acid
+    #                            'ddg_score', # the ddG (change in binding free energy) score
+    #                            'hbond_score', # hydrogen bonding patterns
+    #                            'gyration_radius', # radius of gyration is a measure of the protein's compactness.
+    #                            'sasa_score', # solvent accessible surface area
+    #                            'secondary_structure'
+    #                            ]
+    
+    
+    
+    # converting the data frame to a csv file
+    df.to_csv('example.csv', index=False)
 
 
 ################################################################
@@ -50,6 +82,42 @@ def calculate_secondary_stucture(pose) -> str:
     for i, sec_elem in enumerate(secstruct, start=1):
         sec_strcuture += sec_elem
     return sec_strcuture
+
+################################################################
+def calculate_hbonds_simple(pose) -> int:
+    """
+    Returns the number of hbonds in the passed pose
+    this function calculates the number of hbonds in a simple manner and is computationally less expensive
+    
+    calculates hydrogen bonds based on distance and angle criteria
+    This method is suitable for analyzing small to medium-sized proteins 
+    or for screening large numbers of protein structures for hydrogen bonding.
+    """
+    hbond_set_wt = HBondSet(pose)
+    return hbond_set_wt.nhbonds()
+    
+
+
+################################################################
+def calculate_hbonds_comprehensive(pose) -> int:
+    """
+    Returns the number of hbonds in the passed pose
+    this function calculates the number of hbonds in a more detailed manner
+    
+    This method takes into account the orientation and position of each residue in the protein,
+    as well as the potential for multiple hydrogen bonds to form between different residues.
+    This method is suitable for analyzing large or complex protein structures,
+    or for performing detailed analyses of specific hydrogen bonding interactions
+    """
+    hbond_set = HBondSet()
+    pose.update_residue_neighbors()
+    hbond_set.setup_for_residue_pair_energies(pose, False, False)
+
+
+    total_hbonds = hbond_set.nhbonds()
+    return total_hbonds
+
+
 
 ################################################################
 def mutate_residue(pose, residue_number, new_residue):
