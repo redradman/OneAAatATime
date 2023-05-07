102bd576018ac2156e24e3696a6b6ec292766d08
Radman R <rdmnr@protonmail.com>
2023-05-06 18:23:58 -0700

    add the data for the intein simulation only

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from pyrosetta import *
 from data_utils import *
 
 # initializing PyRosetta
-pyrosetta.init("-out:level 500")
+pyrosetta.init("-out:level 600")
 
 # reading the PDB file
 stay_gold = pyrosetta.pose_from_pdb("StayGold_Intein_v9.pdb")
 gyra_intein = pyrosetta.pose_from_pdb("1am2.pdb")
 
-
 # commencing analysis
 # single_mutation_analysis(stay_gold, "stayGold.csv")
 
-# single_mutation_analysis(gyra_intein, "gyra_intein.csv")
+single_mutation_analysis(gyra_intein, "gyra_intein.csv")
