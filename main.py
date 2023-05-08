a1000e053e05c377814300d8ef3524c3741d4758
Radman R <113865339+redradman@users.noreply.github.com>
2023-05-08 00:02:17 -0700

    add requirement.txt and automate install of non-PyRosetta libraries

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # importing libraries
-from pyrosetta import *
+import os
 from data_utils import *
+try:
+    from pyrosetta import *
+except ImportError:
+    print("PyRosetta is not imported. Make sure that you have installed pyrosetta and that you are in the right environment")
+
+# installing the required libraries
+os.system('pip3 install -r requirements.txt')
 
 # initializing PyRosetta
 pyrosetta.init("-out:level 600")
@@ -12,4 +19,4 @@ gyra_intein = pyrosetta.pose_from_pdb("1am2.pdb")
 # commencing analysis
 # single_mutation_analysis(stay_gold, "stayGold.csv")
 
-single_mutation_analysis(gyra_intein, "gyra_intein.csv")
+single_mutation_analysis(gyra_intein, "intein.csv")
