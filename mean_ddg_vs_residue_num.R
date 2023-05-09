7ee97aed471d9d04300ec4a13981d2012c167b25
Radman R <113865339+redradman@users.noreply.github.com>
2023-05-08 18:02:34 -0700

    fix the residue num r script and its chart

--- mean_ddg_vs_residue_num.R
+++ mean_ddg_vs_residue_num.R
@@ -14,20 +14,19 @@ proteinshort <- proteinshort[, (-c(11))]
 data <- proteinshort %>% 
   filter(type == "mutant") %>% 
   group_by(residue_number) %>% 
-  summarize(mean_ddg = mean(ddg_score), sd_ddg = sd(ddg_score)) %>% 
-  arrange(desc(mean_ddg))
+  summarize(mean_ddg = mean(ddg_score), sd_ddg = sd(ddg_score))
 
 
 # Calculate mean of mean_ddg column
 mean_ddg_mean <- mean(data$mean_ddg)
 
 # Sort the data by mean_ddg in ascending order
-data <- data[order(desc(data$mean_ddg)),]
+Ordereddata <- data[order(desc(data$mean_ddg)),]
 
 # Create the bar chart
-p_all <- ggplot(data, aes(x = factor(residue_number, levels = residue_number), y = mean_ddg)) +
+p_order <- ggplot(Ordereddata, aes(x = factor(residue_number, levels = residue_number), y = mean_ddg)) +
   geom_bar(stat = "identity", fill = "#F28F3B") +
-  scale_y_continuous(expand = c(0, 0), breaks = seq(0, 800, by = 100), limits = c(0, 800)) +
+  scale_y_continuous(expand = c(0, 0), breaks = seq(0, 1700, by = 100), limits = c(0, 1700)) +
   theme(plot.title = element_text(hjust = 0, size = 16, face = "bold"),
         axis.title = element_text(size = 14, face = "bold"),
         axis.text = element_text(size = 12),
@@ -41,12 +40,34 @@ p_all <- ggplot(data, aes(x = factor(residue_number, levels = residue_number), y
   geom_hline(yintercept = mean_ddg_mean, color = "red", size = 1.5, linetype = "dashed") +
   theme(plot.margin = unit(c(1, 1, 1, 0.5), "cm"))
 
-p_all
+p_order
 
 
 # image is simply too large to be produced
-# options(ragg.max_dim = 100000)
-# ggsave("mean_ddg_score_per_residue_num.png", p_all, width = 150, height = 50, dpi = 300, limitsize = FALSE)
+options(ragg.max_dim = 200000)
+ggsave(filename = "charts/mean_ddg_score_per_residue_num_from_max_to_min.png", p_order, width = 100, height = 50, dpi = 300, limitsize = FALSE)
+
+
+p_unorder <- ggplot(data, aes(x = factor(residue_number, levels = residue_number), y = mean_ddg)) +
+  geom_bar(stat = "identity", fill = "#F28F3B") +
+  scale_y_continuous(expand = c(0, 0), breaks = seq(0, 1700, by = 100), limits = c(0, 1700)) +
+  theme(plot.title = element_text(hjust = 0, size = 16, face = "bold"),
+        axis.title = element_text(size = 14, face = "bold"),
+        axis.text = element_text(size = 12),
+        panel.background = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.x = element_line(color = "gray90", size = 0.5),
+        panel.grid.minor.x = element_blank()) +
+  ggtitle("Mean ddg scores for all of the 19 mutations at residue number x") +
+  labs(x = "Residue Number", y = "Mean ddg") +
+  geom_hline(yintercept = mean_ddg_mean, color = "red", size = 1.5, linetype = "dashed") +
+  theme(plot.margin = unit(c(1, 1, 1, 0.5), "cm"))
+
+p_unorder
+
+ggsave(filename = "charts/mean_ddg_score_per_residue_num_from_1.png", p_unorder, width = 100, height = 50, dpi = 300, limitsize = FALSE)
+
 
 
 ###################
